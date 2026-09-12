5b7874c0ffef983f892a05376b0050251f585410
Krish Panchal <panchalkrish202@gmail.com>
2026-09-12 18:15:37 +0530

    feat: complete Python Day 2 control flow

---
+++ day-02/day2.py
@@ -0,0 +1,91 @@
+#completed day 2.py from day 2 learning control flow strcuture 
+
+
+#1. Even or odd 
+# num = int(input("Enter a number : "))
+
+# if num % 2 == 0:
+#     print("The number is even! ")
+# else: 
+#     print("The number is odd!")
+    
+
+#2. positive , negative, zero
+
+# number = int(input("Enter the number : "))
+
+# if number > 0:
+#     print("THe number is positive!")
+# elif number < 0:
+#     print("The number is negative!")
+# else:
+#     print("The number is zero!")
+
+
+# #3. Age checker 
+
+# age = int(input("Enter the age : "))
+
+# if age > 18:
+#     print("The person is adult !") 
+# else:
+#     print("The number is child")
+
+
+#4 Largest of three numbers 
+
+# num1 = int(input("Enter the first number : "))
+# num2 = int(input("Enter the second number : "))
+# num3 = int(input("Enter the third number : "))
+
+# if num1 > num2 and num1 > num3 :
+#     print("Number 1 is greater.")
+# elif num2 > num1 and num2 > num3:
+#     print("Number 2 is greater.")
+# else:
+#     print("Number three is greater.")
+    
+# 5 #Grade calculator 
+
+# Grade = int(input('Enter your grade : '))
+
+# if Grade >= 90:
+#     print("You got an A")
+# elif Grade >= 80:
+#     print("You got an B")
+# elif Grade >= 70:
+#     print("You got an C") 
+# elif Grade >= 60:
+#     print("You got an D")
+# elif Grade <= 59:
+#     print("You have Failed the exam")
+# else: 
+#     print("Invalid score")
+
+
+
+# #6 Login system
+
+# c_username = "dog"
+# c_pass = "0909"
+
+# username = input("Enter a your username : ")
+# passwd = input("Enter the password : ")
+
+# if username == c_username and passwd == c_pass:
+#     print("LOGIN SUCCESSFUL ")
+# elif username != c_username and passwd == c_pass:
+#     print("Incorrect")
+# elif username == c_username and passwd != c_pass:
+#     print("Username is correct but the password is incorrect")
+# else: 
+#     print("INVALID CREDENTIALS")
+
+# 7. Leap year
+leap_year = int(input("Enter a year : "))
+
+if leap_year % 4 == 0:
+    print("Leap year")
+else:
+    print("Not a Leap year")
+
