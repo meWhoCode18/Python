b56cd216240b1b5c94c79e7bd2449ef4e93895bd
Krish Panchal <panchalkrish202@gmail.com>
2026-09-13 12:23:26 +0530

    feat: complete Python Day 3 lists

---
+++ day-03/day3.py
@@ -0,0 +1,142 @@
+
+#learning list
+
+# # Create
+# numbers = [10, 20, 30, 40]
+
+# # Indexing
+# numbers[0]
+# numbers[-1]
+
+# # Change
+# numbers[0] = 100
+
+# # Add
+# numbers.append(50)
+# numbers.insert(1, 15)
+
+# # Remove
+# numbers.remove(20)
+# numbers.pop()
+# numbers.pop(1)
+# del numbers[0]
+# numbers.clear()
+
+# # Length / search
+# len(numbers)
+# 20 in numbers
+
+# # Count / position
+# numbers.count(20)
+# numbers.index(30)
+
+# # Sorting / reversing
+# numbers.sort()
+# numbers.sort(reverse=True)
+# numbers.reverse()
+
+# # Copy
+# new_list = numbers.copy()
+
+# # Slicing
+# numbers[1:4]
+# numbers[:3]
+# numbers[3:]
+# numbers[::2]
+# numbers[::-1]
+
+# # Loop
+# for number in numbers:
+#     print(number)
+
+# # Useful functions
+# min(numbers)
+# max(numbers)
+# sum(numbers)
+
+# # Combine
+# list3 = list1 + list2
+
+# # Nested list
+# matrix = [[1, 2], [3, 4]]
+# matrix[0][1]
+
+#mini project 
+
+# Student Marks analyzer 
+
+students  = ["aranya", "Krish", "Dhruv", "Ved", "Shiv"]
+# #task 1 
+# length = students
+# print(len(length))
+
+
+# #task 2
+# new_student = str(input("Enter the name of the student : "))
+
+# students.append(new_student)
+
+# print(students)
+
+# #task 3 
+
+# students.insert(1, "Kshitij")
+# print(students)
+
+#task 4
+
+
+
+# #task 5
+# remove = students.remove('Krish')
+# print(students)
+
+# #task 6 pop()
+# pop = students.pop(0)
+# print(students)
+
+# #task 7
+# print("Exists : ", "Dhruv" in students)
+
+#task 8 
+# print(students)
+
+#task 9 search
+# for x in students:
+#     print(x)
+
+#task 10 add marks in the second list
+
+second_list = [50, 60, 69, 72, 87]
+# #Highest marks using max()
+# print("Highest marks : ", max(second_list))
+# #lowest marks using min()
+# print("Lowest marks : ", min(second_list))
+# #sum() total marks 
+# print("Total marks : ", sum(second_list))
+# #average 
+
+# average = sum(second_list) / len(second_list)
+# print("Average of the second list :  ", average)
+
+
+# #task 11  learning to ask user input learning reverse and sort 
+# ask = input("Enter the value L = lowest & H = Highest : ")
+# #SORT 
+
+# L = "L"
+# H = "H"
+
+# if ask == "L":
+#     second_list.sort()
+#     print(second_list)
+# elif ask == "H":
+#     second_list.reverse()
+#     print(second_list)
+# else:
+#     print("Invalid input")
+
+copy = second_list.copy()
+print(copy)
+
+
