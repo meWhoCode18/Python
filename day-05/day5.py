867a80f85c5f774847b5a08ecd0405f9e8cce4ec
Krish Panchal <panchalkrish202@gmail.com>
2026-09-15 20:26:55 +0530

    feat: complete Python Day 5 sets

---
+++ day-05/day5.py
@@ -0,0 +1,66 @@
+#set and its operations using github repo 30 days of python
+
+#task 1: 
+#Create a set of 5 programming languages.
+
+# Print it
+# Add "Rust"
+# Print again
+# Print its length
+
+# PL = {"Python", "Java", "C", "C++", "JavaScript"}
+
+# print(PL)
+
+# #added Rust
+# PL.add("Rust")
+# print(PL)
+
+# #print the lengths of the set 
+# length = len(PL)
+# print(length)
+
+
+#task 2 
+
+
+# subjects = {"Python", "Maths", "CN", "FOS", "RDBMS"}
+
+# #removing Maths
+# remove = subjects.discard("Maths")
+# print(subjects)
+
+# #checks whether Python exists or not 
+# print(" Python Exists : ", "Python" in subjects)
+
+# #final set print
+# print(subjects)
+
+
+#task 3
+
+
+
+#union
+# # python = {"Krish", "Dhruv", "Ved", "Shiv"}
+# # java = {"Ved", "Shiv", "Kshitij", "Arjun"}
+
+# # union = python.union(java)
+# # print(union)
+
+# krish_skills = {"Python", "Git", "HTML", "SQL"}
+# friend_skills = {"Python", "Java", "SQL", "Docker"}
+
+# #using intersection to print common elements of set
+# common = krish_skills.intersection(friend_skills)
+# print("Common skills:", common)
+
+# #printing unique things that only one variable holds using difference
+# only_me = krish_skills.difference(friend_skills)
+# print("Skills only I have:", only_me)
+
+
+
+#today i learnt about how set operation works and how union() , difference() and interseciton() works 
+
+# day 5 complete --
