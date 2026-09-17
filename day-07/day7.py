a797545044197d1afb7a5c430bd98fa1dfdee888
Krish Panchal <panchalkrish202@gmail.com>
2026-09-17 17:42:50 +0530

    feat: complete Python Day 7 strings

---
+++ day-07/day7.py
@@ -0,0 +1,13 @@
+#day 7 learning about strings
+
+#task 1 Text Analyzer
+
+sentence = input("Enter a sentence: ")
+
+print("Original : ", sentence) # original
+print("LowerCae : ", sentence.lower()) #lowercase using .lower()
+print("Uppercase : ", sentence.upper()) #uppercase using .upper()
+print("Length : ", len(sentence))
+print("Split words : ", sentence.split())
+
+
