be3515b7aaa975d945a289f9ae8f3ef8fb765ed7
Krish Panchal <panchalkrish202@gmail.com>
2026-09-20 22:33:40 +0530

    chore: initialize Python project

---
+++ day-10-11-12/day10.py
@@ -0,0 +1,10 @@
+# day 10 loops we r doing loops 
+
+# there are mainly two types of loops for example :
+#for loop and a while loop
+for i in range(1,21):
+    print(i)
+
+#print 1 to 50 even numbers using for loop:
+for i in range(2,49,2):
+    print(i)
